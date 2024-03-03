0a655013803bc2942ce6dd123dbd39e2e85ed373
ms6038 <161943813+ms6038@users.noreply.github.com>
2024-03-03 11:09:58 +0400

    Delete Quiz.py

--- Quiz.py
+++
@@ -1,9 +0,0 @@
-# quiz.py
-from flask import Blueprint, render_template
-
-quiz_bp = Blueprint('quiz', __name__)
-
-@quiz_bp.route('/quiz', methods=['GET', 'POST'])
-def quiz():
-    # Quiz logic here
-    return render_template('quiz.html')
